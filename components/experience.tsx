8d84133e0490c1560333972addce5cc60e106ddb
Mani Teja <maniteja2601@gmail.com>
2025-08-11 13:35:29 +0530

    Add Experience section and update project data

---
+++ components/experience.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React from "react";
+import SectionHeading from "./section-heading";
+import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+
+export default function Experience() {
+  const { ref } = useSectionInView("Experience");
+
+  return (
+    <section
+      ref={ref}
+      className="mb-20 scroll-mt-28 max-w-[53rem] text-center sm:mb-40"
+      id="experience"
+    >
+      <SectionHeading>My Experience</SectionHeading>
+      <div className="mt-4 flex flex-col gap-8">
+        {experiencesData.map((item, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: index * 0.1 }}
+            viewport={{ once: true }}
+            className="group mb-3 sm:mb-8 last:mb-0"
+          >
+            <div className="bg-gray-100 dark:bg-white/10 border border-black/5 dark:border-white/5 rounded-lg px-5 py-4 sm:py-6 hover:bg-gray-200 dark:hover:bg-white/20 transition-all duration-300">
+              <div className="pt-4 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50rem] flex flex-col sm:group-even:ml-[18rem]">
+                <div className="flex items-center justify-between mb-2">
+                  <h3 className="text-2xl font-semibold text-left">{item.title}</h3>
+                  <div className="text-sm text-gray-600 dark:text-white/70">
+                    {item.date}
+                  </div>
+                </div>
+                <p className="font-normal text-gray-700 dark:text-white/70 text-left mb-2">
+                  {item.location}
+                </p>
+                <div className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 text-left">
+                  {item.description.split('\n').map((line, index) => (
+                    <p key={index} className="mb-1">
+                      {line}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </section>
+  );
+}
--- lib/data.ts
+++ lib/data.ts
@@ -3,9 +3,10 @@ import { FaXTwitter, FaHashnode } from "react-icons/fa6";
 import { BiLogoLinkedin } from "react-icons/bi";
 import { FiGithub, FiTwitter, FiX } from "react-icons/fi";
 import { link } from "fs";
-import inscribe from '../public/inscribe.png'
-import easyConnect from '../public/easyConnect.png'
-import TH from '../public/TH.png'
+import inscribe from '@/public/inscribe.png';
+import easyConnect from '@/public/easyConnect.png';
+import drawSync from '@/public/drawSync.png';
+import TH from '@/public/TH.png';
 export const socials = [
   {
     name: "GitHub",
@@ -43,6 +44,10 @@ export const links = [
     hash: "#projects",
   },
   {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
     name: "Skills",
     hash: "#skills",
   },
@@ -54,6 +59,24 @@ export const links = [
 
 
 export const projectsData = [
+  {
+    title: "DrawSync",
+    description:
+      "DrawSync is a real-time collaborative drawing app, enabling users to draw and interact in shared rooms.",
+    tags: [
+      "Node.js",
+      "Next.js",
+      "Tailwind",
+      "TypeScript",
+      "WebSockets",
+      "Turborepo",
+      "Redis",
+      "PostgreSQL/Prisma",
+    ],
+    imageUrl: drawSync,
+    link: "https://github.com/Maniteja0126/DrawSync",
+    deployUrl: "https://drawsync-frontend.onrender.com/",
+  },
   {
     title: "Inscribe",
     description:
@@ -102,6 +125,17 @@ export const projectsData = [
     imageUrl: TH,
     link: "https://github.com/Maniteja0126/breeze_travel_frontend",
     deployUrl: "https://touristheaven.vercel.app/",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Fullstack Developer Intern (Backend-focused)",
+    location: "RogueDevs • Remote",
+    description:
+      `• Developed and maintained fullstack features for web applications, improving load times by ~20% \n• Designed and optimized RESTful APIs using Node.js & Rust, enhancing data handling efficiency and reducing response latency \n• Monitored and maintained AWS EC2 production servers, ensuring 99.9% uptime \n• Implemented server health checks, log rotation, and alerts, reducing downtime incidents by 30%`,
+    icon: React.createElement(FiGithub),
+    date: "May 2025 – Present",
   },
 ] as const;
 
